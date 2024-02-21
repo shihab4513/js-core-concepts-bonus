e024920f5c8e04437db98808eadaa76ccbb26cd4
shihab4513 <shihab4513@gmail.com>
2024-02-21 20:33:22 +0600

    first commit

---
+++ object.js
@@ -0,0 +1,20 @@
+
+const Nayok={
+    name:"Sakib",
+    id:121,
+    isSingle:true,
+    friends:["Apu",'RAju','Shakil'],
+    movies:[{name:'No. 1',year:2005},{name:'King khan',year:2001}],
+    act:function(){
+       console.log('Aciting like shalman khan')
+    } ,
+    car:{
+        brand:'Tesla',
+        price:55555555555555555,
+        made:2005,
+        CEO:'Elon Musk',
+        Country:'USA'
+    }
+    
+}
+Nayok.act();
